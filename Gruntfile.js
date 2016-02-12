9efbebaa93ac726831e373e6d20bb6f625cb0c59
Marie Porciuncula <marieporciuncula@Maries-MacBook-Air.local>
2016-02-12 00:11:35 -0800

    commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -40,7 +40,7 @@ module.exports = function (grunt) {
       },
       pages: {
         options: {
-          remote: 'git@github.com:your_github_user/your_webapp.git',
+          remote: 'git@github.com:maricrisp78/Capstone3.git',
           branch: 'gh-pages'
         }
       }
